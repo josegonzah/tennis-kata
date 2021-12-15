ae831f1718bdb114807a1f939a3cd3c457dcbb05
josegonzah <jose.gonzalezh@udea.edu.co>
2021-12-14 22:49:22 -0500

    Katas 1 y 2 refactorizadas

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,8 +1,8 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,63 +13,103 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            scorePlayer1 += 1;
         else
-            m_score2 += 1;
+            scorePlayer2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (scorePlayer1 == scorePlayer2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = getScoreWhenEquals();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (scorePlayer1 >=4 || scorePlayer2 >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = getScoreWhenHigherThan4();
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+            score = getScoreArentEqualOrHigherTo4(score);
+        }
+        return score;
+    }
+
+    private String getScoreWhenHigherThan4() {
+        int minusResult = scorePlayer1 - scorePlayer2;
+        if(Math.abs(minusResult) == 1){
+            return getScoreWhenDifferenceEqualsToAbs1(minusResult);
+        }
+        return  getScoreWhenDiferenceEqualsOrHigherThanAbs2(minusResult);
+    }
+
+    private String getScoreWhenDifferenceEqualsToAbs1(int minusResult) {
+        if (minusResult==1) {
+            return String.format("Advantage %s", player1Name);
+        }
+        return String.format("Advantage %s", player2Name);
+
+    }
+
+    private String getScoreWhenDiferenceEqualsOrHigherThanAbs2(int minusResult) {
+        if (minusResult>=2){
+            return String.format("Win for %s", player1Name);
+        }
+        return String.format("Win for %s", player2Name);
+    }
+
+    private String getScoreWhenEquals() {
+        String score;
+        switch (scorePlayer1)
+        {
+            case 0:
+                score = "Love-All";
+                return score;
+            case 1:
+                score = "Fifteen-All";
+                return score;
+            case 2:
+                score = "Thirty-All";
+                return score;
+            default:
+                score = "Deuce";
+                return score;
+
+        }
+    }
+
+    private String getScoreArentEqualOrHigherTo4(String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) {
+                tempScore = scorePlayer1;
             }
+            else {
+                score +="-"; tempScore = scorePlayer2;
+            }
+            score = getIndividualScore(score, tempScore);
+        }
+        return score;
+    }
+
+    private String getIndividualScore(String score, int tempScore) {
+        switch(tempScore)
+        {
+            case 0:
+                score =String.format("%sLove", score);
+                break;
+            case 1:
+                score =String.format("%sFifteen", score);
+                break;
+            case 2:
+                score =String.format("%sThirty", score);
+                break;
+            case 3:
+                score =String.format("%sForty", score);
+                break;
+            default:
+                break;
         }
         return score;
     }
